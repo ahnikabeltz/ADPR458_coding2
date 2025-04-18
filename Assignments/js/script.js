2affa2086f31fe504bcef0a59f6b3f0a8f2526e5
Ahnika Beltz <Abeltz3@huskers.unl.edu>
2025-04-18 14:52:27 -0500

    final project page one

--- Assignments/js/script.js
+++ Assignments/js/script.js
@@ -1,6 +1,5 @@
 const toggleBtn = document.getElementById("toggle-button");
 const navListItems = document.getElementById("toggle-button");
-console.log("Howdy!");
 
 toggleBtn.addEventListener("click", function (){
     navListItems.classList.toggle("active");
