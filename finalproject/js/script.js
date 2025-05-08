ff1a4559c5bdd42169c42438457be7593c5f0a05
Ahnika Beltz <Abeltz3@huskers.unl.edu>
2025-05-07 20:22:32 -0500

    final additions

---
+++ finalproject/js/script.js
@@ -0,0 +1,9 @@
+$(".slider").slick({
+    // normal options
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: "50px",
+  });
